9de6b6ddcb2401a985296216b909c1b171edbd49
MkLHX <mickael.lehoux@greenponik.com>
2022-02-15 14:48:43 +0100

    edit untrack pycache and add more info on setup

--- setup.py
+++ setup.py
@@ -4,24 +4,23 @@ from setuptools import setup
 VERSION = "2.1.2"
 
 setup(
-    name = "SocksipyChain",
-    version = VERSION,
-    description = "A Python SOCKS/HTTP Proxy module",
+    name="SocksipyChain",
+    version=VERSION,
+    description="A Python SOCKS/HTTP Proxy module",
     install_requires=["six"],
-    long_description = """\
+    long_description="""\
 This Python module allows you to create TCP connections through a chain
 of SOCKS or HTTP proxies without any special effort. It also supports
 TLS/SSL encryption if the OpenSSL modules are installed.
 """,
-     url = "http://github.com/PageKite/SocksiPyChain",
-     author = "Bjarni R. Einarsson",
-     author_email="bre@pagekite.net",
-     license = "BSD",
-     packages=["sockschain"],
-     entry_points = {
-         'console_scripts': [
-             'sockschain = sockschain:Main'
-         ]
-     }
+    url="https://github.com/GreenPonik/PySocksipyChain",
+    author="Bjarni R. Einarsson",
+    author_email="bre@pagekite.net",
+    license="BSD",
+    packages=["sockschain"],
+    entry_points={"console_scripts": ["sockschain = sockschain:Main"]},
+    project_urls={
+        "Source": "https://github.com/pagekite/PySocksipyChain",
+        "Bug Reports": "https://github.com/pagekite/PySocksipyChain/issues",
+    },
 )
-
